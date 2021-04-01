1c49b2b708371645735dfe9542cd7b644c219ee4
Anisha McDowell <anishamcdowell@gmail.com>
2021-04-01 14:32:39 -0400

    index.html made, appendFile function added

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 //Dependencies
 const inquirer = require("inquirer");
+const fs = require("fs");
+
 const Employee = require("../Develop/lib/Employee.js");
 const Engineer = require("../Develop/lib/Engineer.js");
 const Intern = require("../Develop/lib/Intern.js");
@@ -9,12 +11,38 @@ const Manager = require("../Develop/lib/Manager.js");
 inquirer
     .prompt([
         {
-            type: "",
+            type: "input",
+            name: "managerName",
+            message: "What is the team manager's name?",
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the team manager's id?",
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the team manager's email?",
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is the team manager's office number?",
+        },
+        {
+            type: "list",
+            name: "selectMemberType",
+            message: "Which type of team member would you like to add?",
+            choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+        },
+        {
+            type: "input",
             name: "",
             message: "",
         },
         
     ])
     .then (
-
-    )
+        fs.appendFile("index.html", response, (err) => err ? console.error(err) : console.log("Your team has been assembled!"))
+    );
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,5 +1,25 @@
 class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
     
+    getName() {
+        
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getRole() {
+        
+    }
 }
 
 module.exports = Employee;
