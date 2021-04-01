d83f6dde7a5a3c3ed4409bfb0b0eaba2c9b5bb35
Anisha McDowell <anishamcdowell@gmail.com>
2021-04-01 14:20:31 -0400

    Start of employee class; index.js dependencies made; start of inquirer prompts

---
+++ Develop/index.js
@@ -0,0 +1,20 @@
+//Dependencies
+const inquirer = require("inquirer");
+const Employee = require("../Develop/lib/Employee.js");
+const Engineer = require("../Develop/lib/Engineer.js");
+const Intern = require("../Develop/lib/Intern.js");
+const Manager = require("../Develop/lib/Manager.js");
+
+//CL Prompts
+inquirer
+    .prompt([
+        {
+            type: "",
+            name: "",
+            message: "",
+        },
+        
+    ])
+    .then (
+
+    )
