cb4a74e713e2a53059f7cecf8ddabf834943bb42
Anisha McDowell <anishamcdowell@gmail.com>
2021-04-03 00:58:26 -0400

    Questions awaiting response but running at same time. Need to make each team member prompt synchronous

--- Develop/index.js
+++ Develop/index.js
@@ -8,13 +8,125 @@ const Employee = require("../Develop/lib/Employee.js");
 const Engineer = require("../Develop/lib/Engineer.js");
 const Intern = require("../Develop/lib/Intern.js");
 const Manager = require("../Develop/lib/Manager.js");
+const { listenerCount } = require("events");
 
 
-//Employee variables
-// const manager = new Employee(`${data.managerName}, ${data.managerId}, ${data.managerEmail}`, "Manager")
-// const engineer = new Employee(`${engineerName}, ${engineerId}, ${engineerEmail}`, "Engineer");
-// const intern = new Employee(`${internName}, ${internId}, ${internEmail}`, "Intern");
 
+//Prompt user to begin assembling team
+console.log("ASSEMBLE YOUR TEAM...");
+
+//Questions objs/arrays
+//Choose a team member type
+const teamQuestion =
+{
+    type: "list",
+    name: "selectMemberType",
+    message: "Which type of team member would you like to add next?",
+    choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+}
+//Manager questions
+const managerQuestions = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "What is the team manager's name?",
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is the team manager's id?",
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the team manager's email?",
+    },
+    {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the team manager's office number?",
+    },
+]
+//Engineer questions
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "What is the team engineer's name?",
+    },
+
+    {
+        type: "input",
+        name: "engineerId",
+        message: "What is the team engineer's id?",
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the team engineer's email?",
+    },
+    {
+        type: "input",
+        name: "engineerGitHub",
+        message: "What is the team enginner's github username?",
+    },
+
+]
+//Intern questions
+const internQuestions = [
+    {
+        type: "input",
+        name: "internName",
+        message: "What is the team intern's name?",
+    },
+
+    {
+        type: "input",
+        name: "internId",
+        message: "What is the team intern's id?",
+    },
+    {
+        type: "input",
+        name: "internEmail",
+        message: "What is the team intern's email?",
+    },
+    {
+        type: "input",
+        name: "internSchool",
+        message: "What school did the team intern attend?",
+    },
+
+]
+
+
+//Prompt all team questions
+async function askManagerQuestions(managerQuestions) {
+    const managerAnswers = await inquirer.prompt(managerQuestions);
+    console.log("Manger added");
+    return managerAnswers;
+};
+async function askEngineerQuestions(engineerQuestions) {
+    const engineerAnswers = await inquirer.prompt(engineerQuestions);
+    console.log("Engineer added");
+    return engineerAnswers;
+};
+async function askInternQuestions(internQuestions) {
+    const internAnswers = await inquirer.prompt(internQuestions);
+    console.log("Intern added");
+    return internAnswers;
+};
+
+const go = Promise.all([askManagerQuestions(managerQuestions), askEngineerQuestions(engineerQuestions), askInternQuestions(internQuestions)])
+.then((answers) => {
+    console.log(answers);
+});
+  
+// askManagerQuestions(managerQuestions);
+// askEngineerQuestions(engineerQuestions);
+// askInternQuestions(internQuestions);
+
+
+//Generate HTML based on user data
 const generateHtml = data => `<html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -75,74 +187,16 @@ const generateHtml = data => `<html lang="en">
     </main>
     <script src="../index.js"></script>`;
 
-//CLI Prompts
-// console log prompt
-console.log("ASSEMBLE YOUR TEAM..");
-
-//prompt manager questions
-inquirer.prompt([
-    {
-        type: "input",
-        name: "managerName",
-        message: "What is the team manager's name?",
-    },
-    {
-        type: "input",
-        name: "managerId",
-        message: "What is the team manager's id?",
-    },
-    {
-        type: "input",
-        name: "managerEmail",
-        message: "What is the team manager's email?",
-    },
-    {
-        type: "input",
-        name: "managerOfficeNumber",
-        message: "What is the team manager's office number?",
-    },
-])
 //then append data to index.html file
-.then((data) => {
+const newTeam = (data) => {
     fs.appendFile("index.html", generateHtml(data), (err) =>
-    err ? console.error(err) : 
-    inquirer.prompt(
-        //select next user type
-        {
-            type: "list",
-            name: "selectMemberType",
-            message: "Which type of team member would you like to add next?",
-            choices: ["Engineer", "Intern", "I don't want to add any more team members"],
-            filter: (value) => {
-                return value;
-            }
-        },
-    )
-    // [
-    //     {
-    //         type: "input",
-    //         name: "internName",
-    //         message: "What is the team intern's name?",
-    //     },
+    err ? console.error(err) : console.log("TEAM ASSEMBLED!"));
+}
 
-    //     {
-    //         type: "input",
-    //         name: "internId",
-    //         message: "What is the team intern's id?",
-    //         when: "(data) => { data.choices === 'Intern' }",
-    //     },
-    //     {
-    //         type: "input",
-    //         name: "internEmail",
-    //         message: "What is the team intern's email?",
-    //         when: "(data) => { data.choices === 'Intern' }",
-    //     },
-    //     {
-    //         type: "input",
-    //         name: "internSchool",
-    //         message: "What school did the team intern attend?",
-    //         when: "(data) => { data.choices === 'Intern' }",
-    //     },
-    // ])
-);
-})
+// //Ask questions
+// //What team members to add
+// //Manager questions
+// const askManagerQuestions = async (managerQuestions) => {
+//     const answers = await inquirer.prompt(managerQuestions);
+//     return answers;
+// }
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,21 +1,18 @@
 const Employee = require("./Employee");
 
 class Intern extends Employee {
-    class Intern extends Employee {
-        constructor(name, id, school) {
-            super(name, id, email);
-            this.school = school;
-        }
-    
-        askSchool() {
-            return this.school;
-        }
-    
-        getRole() {
-            return "Intern";
-        }
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+
+    askSchool() {
+        return this.school;
+    }
+
+    getRole() {
+        return "Intern";
     }
 }
-
 
 module.exports = Intern;
